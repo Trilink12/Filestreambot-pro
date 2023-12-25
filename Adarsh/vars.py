10293cad6fc14d99ffec4eb938ac07b0f3578062
Trilink12 <154373433+Trilink12@users.noreply.github.com>
2023-12-25 18:04:52 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -35,6 +35,6 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', ''))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', ''))
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://trilink1212:19JmwI3CHKVvo6Ew@cluster0.f8y7dvy.mongodb.net/?retryWrites=true&w=majority'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'backup_channelc))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
